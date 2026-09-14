2c83ca809f15a397d27836301df679dd92e811de
Samar HICHRI <113449687+samarhichri@users.noreply.github.com>
2026-09-14 16:52:51 +0200

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -4,4 +4,7 @@ DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_USER = os.getenv("DB_USER", "root")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
 DB_NAME = os.getenv("DB_NAME", "todo_app")
-DB_PORT = int(os.getenv("DB_PORT", 3306))
+try:
+    DB_PORT = int(os.getenv("DB_PORT", "3306"))
+except ValueError:
+    raise ValueError("DB_PORT must be a valid integer")
