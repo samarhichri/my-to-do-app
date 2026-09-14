f0e049747bc9666198d09072c6048d3f38607d52
Samar HICHRI <113449687+samarhichri@users.noreply.github.com>
2026-09-14 16:51:56 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,8 @@
 from flask import Flask
 from routes import api
-from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
     app.register_blueprint(api)
     return app
 
